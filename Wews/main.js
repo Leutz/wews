32e12408911b771f5a58896b5cb9648346e5a7a9
Madalin <leonte_madalin@yahoo.com>
2019-03-22 00:19:55 +0200

    Finished articles layout, responsive

--- Wews/main.js
+++ Wews/main.js
@@ -1,27 +1,17 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    if (innerWidth > 1200) {
+        document.getElementById("main").style.marginLeft = "250px";
+
+    }
     /*document.body.style.backgroundColor = "rgba(0,0,0,0.4)";*/
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    if (innerWidth > 1200) {
+        document.getElementById("main").style.marginLeft = "0";
+
+    }
     /*document.body.style.backgroundColor = "white";*/
-}
-
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
 }
