aa426852d86157a70563cc30c97b5eef8d136d5e
Madalin <leonte_madalin@yahoo.com>
2019-03-22 09:27:34 +0200

    Minor fixes

--- Wews/animations.js
+++ Wews/animations.js
@@ -2,16 +2,12 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     if (innerWidth > 1200) {
         document.getElementById("main").style.marginLeft = "250px";
-
     }
-    /*document.body.style.backgroundColor = "rgba(0,0,0,0.4)";*/
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     if (innerWidth > 1200) {
         document.getElementById("main").style.marginLeft = "0";
-
     }
-    /*document.body.style.backgroundColor = "white";*/
 }
