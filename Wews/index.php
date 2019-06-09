277496f913bfa9095269b2ff0002ea8a84cca4c1
Mindru Madalin <40139779+zornet@users.noreply.github.com>
2019-06-09 17:38:24 +0300

    Minore.

--- Wews/index.php
+++ Wews/index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require "database.php";
 if( !isset( $_SESSION['logged'] ) ) {
 echo '<script type="text/javascript">';
 echo 'alert("Nu esti conectat! Vei fi redirectionat spre pagina de logare!");';
@@ -7,7 +8,24 @@ echo 'window.location = "login/login.php";';
 echo '</script>';
 }
 
- ?>
+if(!empty($_POST['bookmark']))
+{
+  echo $_POST['bookmark'];
+  $sql = "INSERT into articles (id,titlu,link,topic,created_at,updated_at) values (id,:title,:link,:topic,created_at,updated_at)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':title', $entry->title);
+  $stmt->bindParam(':link', $entry->link);
+  $stmt->bindParam(':topic', $entry->description);
+  if($stmt->execute())
+  {
+  echo '<script type="text/javascript">';
+  echo 'alert("Ai adaugat cu succes.");';
+  echo '</script>';}
+  unset($_POST['bookmark']);
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 
@@ -117,22 +135,6 @@ echo '</script>';
                                   <form method="post">
                                     <input class="top-left" type="submit" name="bookmark" id="test" value="BOOKMARK"><br/>
                                   </form>
-                                  <?php
-                                  if(!empty($_POST['bookmark']))
-                                  {
-                                    $sql = "INSERT into articles (id,titlu,link,topic,created_at,updated_at) values (id,:title,:link,:topic,created_at,updated_at)";
-                                    $stmt = $conn->prepare($sql);
-                                    $stmt->bindParam(':title', $entry->title);
-                                    $stmt->bindParam(':link', $entry->link);
-                                    $stmt->bindParam(':topic', $entry->description);
-                                    if($stmt->execute())
-                                    {
-                                    echo '<script type="text/javascript">';
-                                    echo 'alert("Ai adaugat cu succes.");';
-                                    echo '</script>';}
-                                    unset($_POST['bookmark']);
-                                  } ?>
-
                                   <div class="top-right" ><button class="readBtn" onclick="window.open('<?= $entry->link ?>','_blank')"><b>READ</b></button></div>
                                   <div class="bottom">
                                       <h4 id="title"><strong><?= $entry->title ?></strong></h4>
