55454e4e7f8abc220897ef85b10bcc4b6e7a53e9
Mindru Madalin <40139779+zornet@users.noreply.github.com>
2019-06-10 20:18:51 +0300

    Update database.php

--- Wews/database.php
+++ Wews/database.php
@@ -1,11 +1,12 @@
 <?php
-$server = 'db4free.net';
+$server = 'mysql-2937-0.cloudclusters.net';
 $username = 'proiecttw';
 $password = '2u3pu2u3pu';
 $database = 'proiecttw';
+$port="10017";
 
 try{
-	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+	$conn = new PDO("mysql:host=$server;dbname=$database;port=$port;", $username, $password);
 } catch(PDOException $e){
 	die( "Connection failed: " . $e->getMessage());
 }
